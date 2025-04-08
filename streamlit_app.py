072ff5db57115524ca17f74bf2351788fd853d04
LuckyThundarr <111292462+luckythundarr@users.noreply.github.com>
2025-04-08 13:43:14 -0400

    Update streamlit_app.py
    typo date -> data

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + search_on)
-        st_df = st.dataframe(date = smoothiefroot_response.json(), use_container_width = True)
+        st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
     
     #st.write(ingredients_string)
 
